1e57a30201bff6f47e71a56c435e690aece1ee3d
Anup-cloud28 <anupkabberalli28@gmail.com>
2026-07-16 00:01:24 +0530

    created SG

--- ec2-practise/main.tf
+++ ec2-practise/main.tf
@@ -5,4 +5,31 @@ provider "aws" {
 resource "aws_instance" "AkTestinstance" {
     ami = "ami-0b6d9d3d33ba97d99"
     instance_type = "t3.micro"
+    security_groups = []
+}
+
+resource "aws_security_group" "TestSG" {
+    name = "Allow Http"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]  
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] 
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"] 
+    }
+  
 }
